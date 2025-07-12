812be2e85ac2204124fbc49be3ec3edecf6948eb
taku1029-dev <taku1029.dev@gmail.com>
2025-07-12 12:56:13 +0900

    Output executables under 'build' dir

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -29,7 +29,7 @@ return {
         program = function()
           local current_file = vim.fn.expand('%:p')
           local file_name = vim.fn.expand('%:t:r') -- without file extension
-          local output_file = vim.fn.expand('%:p:h') .. '/' .. file_name
+          local output_file = vim.fn.expand('%:p:h') .. '/build/' .. file_name
 
           local compile_cmd = string.format('g++ -g -o %s %s', output_file, current_file)
           print("Compiling: " .. compile_cmd)
@@ -46,13 +46,39 @@ return {
         stopOnEntry = false,
       }
     }
-    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.c = {
+      {
+        name = "Build and Debug",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          local current_file = vim.fn.expand('%:p')
+          local file_name = vim.fn.expand('%:t:r') -- without file extension
+          local output_file = vim.fn.expand('%:p:h') .. '/build/' .. file_name
+
+          local compile_cmd = string.format('gcc -g -o %s %s', output_file, current_file)
+          print("Compiling: " .. compile_cmd)
+          local result = vim.fn.system(compile_cmd)
+          if vim.v.shell_error ~= 0 then
+            print("Compilation failed")
+            print(result)
+            return nil
+          end
+          print("Compilation Successfull!\nOutput: " .. output_file)
+          return output_file
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+      }
+    }
+
 
   end,
   keys = {
     {"b", "<cmd>DapToggleBreakpoint<cr>", mode = {"n"}, desc = "Toggle Breakpoint"},
     {"<F5>", "<cmd>DapContinue<cr>", mode = {"n"}, desc = "Start Debugging"},
-    {"<F9>", "<cmd>DapStepOver<cr>", mode = {"n"}, desc = "Step Over"},
-    {"<F10>", "<cmd>DapStepInto<cr>", mode = {"n"}, desc = "Step Into"},
+    {"<F10>", "<cmd>DapStepOver<cr>", mode = {"n"}, desc = "Step Over"},
+    {"<F11>", "<cmd>DapStepInto<cr>", mode = {"n"}, desc = "Step Into"},
+    {"<F12>", "<cmd>DapTerminate<cr>", mode = {"n", "i", "t"}, desc = "Step Into"},
   },
 }
